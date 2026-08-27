3a797709d9831b16818e2e02aedd7e17425ffa97
Mussa Yousef <mussayousef1@hotmail.com>
2026-08-27 01:05:29 +0100

    Remove runtime dependency installer

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,1 @@
-import subprocess
-import sys
-
-
-try:
-    import plotly  # noqa: F401
-except ModuleNotFoundError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "plotly>=5.24.1"])
-
-
 from dashboard.app import *  # noqa: F401,F403
